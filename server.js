d6c579283f58059013e36d7173c95a4040de027a
Un-index <68165727+Un-index@users.noreply.github.com>
2021-09-10 19:16:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
  * note check logs frequently and refresh the editor when using Glitch for hosting
  */
 
-// it is extremely easy to make the keys 5 char and unique instead
+// it is extremely easy to make the keys 5 char and unique instead of just a number that'll be appended to the url
 // of using numbers,
 // however I am not implementing that but I'll leave some code  to
 // demonstrate one way to generate the key
 /*
       
-    const {randomBytes} = require("crypto")
+    const {randomBytes} = require('crypto')
      randomBytes(3, (err, buf) => {
       if (err) throw err;
       let 5char = buf.toString('hex').substr(0,5)
@@ -27,21 +27,21 @@
 */
 
 // NOTE: the .env is scrubbed when remixed, so it is safe to store
-// api keys and stuff
+// api keys and stuff there
 
 // TODO: clear db via signing in and visiting https://cloud.mongodb.com/
 // via the left sidebar:  Deployment > Databases > View Collections
 
-require("dotenv").config();
+require('dotenv').config();
 
-const dns = require("dns");
-const express = require("express");
-const cors = require("cors");
+const dns = require('dns');
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
 
 async function main() {
-  const mongoose = require("mongoose");
+  const mongoose = require('mongoose');
   try {
     await mongoose.connect(process.env.mongoDbURI, {
       useNewUrlParser: true,
@@ -52,15 +52,15 @@ async function main() {
   }
 
   const connection = mongoose.connection;
-  connection.once("open", function() {
-    console.log("connected");
+  connection.once('open', function() {
+    console.log('connected');
   });
 
   const Schema = mongoose.Schema;
 
   // store one empty entry so the rest can start from 1 as in the sample https://url-shortener-microservice.freecodecamp.rocks/api/shorturl/0
 
-  const aModel = connection.model("n", new Schema({ _id: String, n: Number }));
+  const aModel = connection.model('n', new Schema({ _id: String, n: Number }));
 
   const urlEntrySchema = new Schema({
     _id: Number,
@@ -68,33 +68,32 @@ async function main() {
     short_url: String
   });
 
-  const urlEntryModel = connection.model("urlEntry", urlEntrySchema);
+  const urlEntryModel = connection.model('urlEntry', urlEntrySchema);
 
   app.use(cors());
   app.use(express.urlencoded());
   app.use(express.json());
-  app.use("/public", express.static(`${process.cwd()}/public`));
+  app.use('/public', express.static(`${process.cwd()}/public`));
 
   // log all urlEntryModel documents
   // urlEntryModel.find({}, (err, document) => {
   //  console.log(document);
   // })
 
-  app.get("/api/shorturl/:id", async function(req, res) {
+  app.get('/api/shorturl/:id', async function(req, res) {
     const id = req.params.id;
 
     // check if it is a valid id
     if (isNaN(id)) {
-      return res.send({ error: "Wrong format" });
+      return res.send({ error: 'Wrong format' });
     }
 
-    // return urlEntryModel.findOne({ id: Number(request.params.id) }, function (err, document) {
     const document = await urlEntryModel.findById(id);
     if (document) {
       console.log(
-        "found urlEntryModel, id: ",
+        'found urlEntryModel, id: ',
         req.params.id,
-        "og url: ",
+        'og url: ',
         document.original_url
       );
 
@@ -102,31 +101,33 @@ async function main() {
     }
 
     // if the document wasn't found, tell them it wasn't
-    res.send({ error: "No short URL found for the given input" });
+    res.send({ error: 'No short URL found for the given input' });
   });
 
-  app.get("/", function(req, res) {
-    res.sendFile(process.cwd() + "/views/index.html");
+  app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
   });
 
-  app.get("/api/hello", function(req, res) {
-    res.json({ greeting: "hello API" });
+  app.get('/api/hello', function(req, res) {
+    res.json({ greeting: 'hello API' });
   });
 
   // made it async
   async function getNextNandUse(callback, url) {
     // if url already exists then return false
     const document = await urlEntryModel.find({ original_url: url });
-    // console.log("document currently = ", document)
+    // console.log('document currently = ', document)
     if (document.length !== 0) {
       console.log(
-        "document already exists with this original_url: ",
+        'document already exists with this original_url: ',
         document[0].original_url
       );
       callback(false, document[0].short_url);
       return;
     }
 
+    // increment the counter in the 'n' collection every time we need to save a document in the urlEntryModel collection 
+        
     let nValue;
     aModel.findOneAndUpdate(
       {},
@@ -140,18 +141,18 @@ async function main() {
     return Number(nValue);
   }
 
-  app.post("/api/shorturl", function(req, res) {
+  app.post('/api/shorturl', function(req, res) {
     const url = req.body.url;
     var urlLToParse;
     try {
       urlLToParse = new URL(url);
     } catch (e) {
-      res.send({ error: "Invalid URL" });
+      res.send({ error: 'Invalid URL' });
       return;
     }
     // could be undefined
     if (!urlLToParse) {
-      res.send({ error: "Invalid URL" });
+      res.send({ error: 'Invalid URL' });
       return;
     }
 
@@ -160,7 +161,7 @@ async function main() {
     // https://nodejs.org/api/dns.html#dns_dns_lookup_hostname_options_callback
     dns.lookup(hostName, err => {
       if (err) {
-        res.send({ error: "Invalid URL" });
+        res.send({ error: 'Invalid URL' });
         return;
       }
     });
@@ -168,13 +169,13 @@ async function main() {
     let getNNow = (nVal, existingShortUrl) => {
       if (!nVal) {
         console.log(
-          "short url exists, sending that in the response: (%s)",
+          'short url exists, sending that in the response: (%s)',
           existingShortUrl
         );
         res.send({ original_url: url, short_url: existingShortUrl });
         return;
       }
-      console.log("will use n: ", nVal);
+      console.log('will use n: ', nVal);
       const urlEntry = new urlEntryModel({
         _id: nVal,
         original_url: url,
